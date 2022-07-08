d41bf3c6f0b2fda590f3f3e4398b2f244d56d1c4
bishop254 <karanjachiuri@gmail.com>
2022-07-08 15:09:23 +0300

    Added SQL code to create three more tables for our category, product and bill

--- Backend/DB/tables.sql
+++ Backend/DB/tables.sql
@@ -20,4 +20,32 @@ values
         'true',
         'admin'
     );
-    
+
+create table category(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL UNIQUE,
+    primary key(id)
+);
+
+create table product(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL UNIQUE,
+    categoryID int NOT NULL,
+    description varchar(255),
+    price int,
+    status varchar(20),
+    primary key(id)
+);
+
+create table bill(
+    id int NOT NULL AUTO_INCREMENT,
+    uuid varchar(200) NOT NULL UNIQUE,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    contactNumber varchar(20) NOT NULL,
+    paymentMethod varchar(50) NOT NULL,
+    total int NOT NULL,
+    productDetails JSON DEFAULT NULL,
+    createdBy varchar(255) NOT NULL,
+    primary key(id)
+);
