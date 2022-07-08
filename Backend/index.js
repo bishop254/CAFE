6aa49a69babab1e08ac61c1e72ca5a554f963bd1
bishop254 <karanjachiuri@gmail.com>
2022-07-08 15:08:09 +0300

    Added new routes for category, product and bill

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const connection = require("./connection");
 const userRoute = require("./routes/user");
+const categoryRoute = require("./routes/category");
+const productRoute = require("./routes/product");
+const billRoute = require("./routes/bill");
 
 const app = express();
 
@@ -16,5 +19,8 @@ app.use(
 app.use(express.json());
 
 app.use("/user", userRoute);
+app.use("/category", categoryRoute);
+app.use("/product", productRoute);
+app.use("/bill", billRoute);
 
 module.exports = app;
