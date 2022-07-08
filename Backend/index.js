3d66cc804e412f39fb2e79227b6df4e537ecf1c1
bishop254 <karanjachiuri@gmail.com>
2022-07-08 15:44:22 +0300

    Added the route to the dashboard

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const userRoute = require("./routes/user");
 const categoryRoute = require("./routes/category");
 const productRoute = require("./routes/product");
 const billRoute = require("./routes/bill");
+const dashboardRoute = require("./routes/dashboard");
 
 const app = express();
 
@@ -22,5 +23,6 @@ app.use("/user", userRoute);
 app.use("/category", categoryRoute);
 app.use("/product", productRoute);
 app.use("/bill", billRoute);
+app.use("/dashboard", dashboardRoute);
 
 module.exports = app;
