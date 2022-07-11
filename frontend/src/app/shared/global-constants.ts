0f8e6f3881706497e6708f1e61f519b80cfe3f77
bishop254 <karanjachiuri@gmail.com>
2022-07-11 12:08:14 +0300

    Added a few error handling messages that can be accessed all-over the application.

--- frontend/src/app/shared/global-constants.ts
+++ frontend/src/app/shared/global-constants.ts
@@ -2,6 +2,14 @@ export class GlobalConstants {
   public static genericError: string =
     'Something went wrong!! PLease try again!!';
 
+  public static unauthorized: string =
+    'You are not authorized to view this page!';
+
+  public static productExistError: string = 'Product already exists';
+
+  public static productAdded: string = 'Product added successfully';
+  
+
   public static nameRegex: string = '[a-zA-Z0-9 ]*';
 
   public static emailRegex: string =
