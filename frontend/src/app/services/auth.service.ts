cc9c1f0721d62adc2f87c10c7127c653273eee7a
bishop254 <karanjachiuri@gmail.com>
2022-07-11 12:14:31 +0300

    An auth service to check if a token is available in the localStorage.

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private router: Router) {}
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
